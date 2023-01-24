9ac36c8d6d6d415581c780a935e8fec600000128
Daniel9822 <dhdany64@gmail.com>
2023-01-24 19:06:31 -0400

    admin ❤️

--- src/Administrador/createAdmin.js
+++ src/Administrador/createAdmin.js
@@ -7,10 +7,11 @@ const createAdmin = async () => {
     try {
         const pass = await bcrypt.hash(SECRET_PASSOWORD_ADMIN, 10);
         await Admin.findOrCreate({
-            mail: 'admin@andromeda.com',
-            password: pass
-            
-        })
+            where: {
+                mail: 'admin@andromeda.com',
+                password: pass
+            },
+        });
         return "admin created"; 
     } catch (error) {
           return error.message
