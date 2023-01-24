a0911d73498a4451a95393ab3cf003843ae9496c
Daniel9822 <dhdany64@gmail.com>
2023-01-24 17:06:55 -0400

    firt commit

--- src/app.js
+++ src/app.js
@@ -19,6 +19,14 @@ server.use(cors())
 
 server.use(morgan('dev'))
 server.use(express.json())
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+  
 server.use(gamesRouter)
 server.use(gameFavoriteRouter) 
 server.use(loginRouter)
